3cd06351950d489c9e634fecc378a77ccbdc688d
Andy J <andrew.johncey@outlook.com>
2020-04-09 00:08:03 +1000

    add api notes routes

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,18 @@
 const path = require("path");
 const express = require("express");
+const fs = require("fs").promises;
+const bodyParser = require("body-Parser");
+const shortid = require("shortid");
 
 // const apiRoutes = require("./routes/apiRoutes");
 // const htmlRoutes = require("./routes/htmlRoutes");
 
 // create an express server
 const app = express();
+
+const dbFilePath = path.resolve(__dirname, '.', 'db', 'db.JSON');
+
+app.use(express.static("public"));
 
 // set an initial port
 const PORT = process.env.PORT || 3000;
@@ -23,6 +30,25 @@ app.get('/notes', (_, res) => {
     res.sendFile(filePath);
 });
 
+app.get('/api/notes', async (_, res) => {
+    const fileData = await fs.readfile(dbFilePath, "utf-8");
+    const data = JSON.parse(fileData);
+    res.json(data);
+
+});
+
+//id, title, text
+app.post('/api/notes', async (req, res) => {
+    const { title, text } = req.body;
+        
+    const fileData = await fs.readfile(dbFilePath, "utf-8");
+    const data = JSON.parse(fileData);
+
+    // res.json(data);
+    res.end();
+});
+ 
+
 app.use('*', (_, res) => {
         res.redirect('/');
 });
@@ -30,8 +56,9 @@ app.use('*', (_, res) => {
 // set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.use(express.static("public"));
 // Router
 // require("./routes/apiRoutes")(app);
 // require("./routes/htmlRoutes")(app);
